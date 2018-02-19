6a622eebeb6199b766227bfa6080d8e367d6a0ea
Egor Dorichev <egordorichev@gmail.com>
2018-02-19 11:59:38 +0300

    init

---
+++ android/src/org/rexellentgames/dungeon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.rexellentgames.dungeon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.rexellentgames.dungeon.Dungeon;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Dungeon(), config);
+	}
+}
---
+++ ios/src/org/rexellentgames/dungeon/IOSLauncher.java
@@ -0,0 +1,22 @@
+package org.rexellentgames.dungeon;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.rexellentgames.dungeon.Dungeon;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Dungeon(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
