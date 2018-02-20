ec34735f78f102434e0a231f0f47ee9ed68a75ba
Egor Dorichev <egordorichev@gmail.com>
2018-02-20 10:26:21 +0300

    Some generation improvements

--- core/src/org/rexellentgames/dungeon/entity/level/painter/HolePainter.java
+++ core/src/org/rexellentgames/dungeon/entity/level/painter/HolePainter.java
@@ -7,7 +7,6 @@ import org.rexellentgames.dungeon.entity.level.Terrain;
 
 public class HolePainter extends Painter {
 	public static void paint(Level level, Room room) {
-		fill(level, room, Terrain.WALL);
 		fill(level, room.left + 1, room.top + 1, room.getWidth() - 2, room.getHeight() - 2, Terrain.FLOOR);
 		fill(level, room.left + 2, room.top + 2, room.getWidth() - 4, room.getHeight() - 4, Terrain.EMPTY);
 
