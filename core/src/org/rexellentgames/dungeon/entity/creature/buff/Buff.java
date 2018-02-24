e7a80219b932eb0d4f4b5787d3a3e822a550cf03
Egor Dorichev <egordorichev@gmail.com>
2018-02-24 20:15:47 +0300

    Working on the buff

---
+++ core/src/org/rexellentgames/dungeon/entity/creature/buff/Buff.java
@@ -0,0 +1,18 @@
+package org.rexellentgames.dungeon.entity.creature.buff;
+
+public class Buff {
+	protected String name;
+	protected String description;
+	protected int sprite;
+	protected float duration;
+	protected float time;
+	protected boolean bad = false;
+
+	public Buff(float duration) {
+		this.duration = duration;
+	}
+
+	public Buff() {
+		
+	}
+}
---
+++ core/src/org/rexellentgames/dungeon/entity/creature/buff/BurningBuff.java
@@ -0,0 +1,10 @@
+package org.rexellentgames.dungeon.entity.creature.buff;
+
+public class BurningBuff extends Buff {
+	{
+		duration = 30.0f;
+		name = "Burning";
+		description = "You are on fire!";
+		sprite = 0;
+	}
+}
