dfc5c42bb468fbe709081558df9149f9effacd6e
Egor Dorichev <egordorichev@gmail.com>
2018-02-25 15:23:38 +0300

    Open / close inventory

--- core/src/org/rexellentgames/dungeon/entity/creature/inventory/UiInventory.java
+++ core/src/org/rexellentgames/dungeon/entity/creature/inventory/UiInventory.java
@@ -3,22 +3,28 @@ package org.rexellentgames.dungeon.entity.creature.inventory;
 import org.rexellentgames.dungeon.assets.Graphics;
 import org.rexellentgames.dungeon.entity.Entity;
 import org.rexellentgames.dungeon.entity.item.Item;
+import org.rexellentgames.dungeon.game.input.Input;
+import org.rexellentgames.dungeon.util.Log;
 
 public class UiInventory extends Entity {
 	private Inventory inventory;
+	private boolean open = false;
 
 	public UiInventory(Inventory inventory) {
 		this.inventory = inventory;
+		this.alwaysActive = true;
 	}
 
 	@Override
 	public void update(float dt) {
-
+		if (Input.instance.wasPressed("toggle_inventory")) {
+			this.open = !this.open;
+		}
 	}
 
 	public void renderUi() {
 		for (int x = 0; x < 6; x++) {
-			for (int y = 0; y < 4; y++) {
+			for (int y = 0; y < (this.open ? 4 : 1); y++) {
 				int xx = x * 25 + 1;
 				int yy = y * 25 - 7;
 				int i = x + y * 6;
