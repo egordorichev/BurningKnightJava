6ae7ce73bc7ab7993988f5854f3e88646459efa4
Egor Dorichev <egordorichev@gmail.com>
2018-02-26 18:48:48 +0300

    New sword swing animation

--- core/src/org/rexellentgames/dungeon/entity/creature/mob/Knight.java
+++ core/src/org/rexellentgames/dungeon/entity/creature/mob/Knight.java
@@ -43,26 +43,34 @@ public class Knight extends Mob {
 		this.vel.mul(0f);
 
 		if (this.target != null) {
-			if (this.t % 1 <= 0.017) {
-				this.point = this.getCloser(this.target);
-			}
+			float dx = this.target.x - this.x;
+			float dy = this.target.y - this.y;
+			float d = (float) Math.sqrt(dx * dx + dy * dy);
 
-			if (this.point != null) {
-				float dx = this.point.x - this.x;
-				float dy = this.point.y - this.y;
-				float d = (float) Math.sqrt(dx * dx + dy * dy);
-
-				if (d < 1) {
-					this.x = this.point.x;
-					this.y = this.point.y;
-
-					this.point = this.getCloser(this.target);
-				} else {
-					this.vel.x = dx / d * this.speed;
-					this.vel.y = dy / d * this.speed;
-				}
+			if (d < 16) {
+				// attack
 			} else {
-				this.point = this.getCloser(this.target);
+				if (this.t % 1 <= 0.017) {
+					this.point = this.getCloser(this.target);
+				}
+
+				if (this.point != null) {
+					dx = this.point.x - this.x;
+					dy = this.point.y - this.y;
+					d = (float) Math.sqrt(dx * dx + dy * dy);
+
+					if (d < 1) {
+						this.x = this.point.x;
+						this.y = this.point.y;
+
+						this.point = this.getCloser(this.target);
+					} else {
+						this.vel.x = dx / d * this.speed;
+						this.vel.y = dy / d * this.speed;
+					}
+				} else {
+					this.point = this.getCloser(this.target);
+				}
 			}
 		} else {
 			this.assignTarget();
