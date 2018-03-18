ada8713c43fb37b6cebd51949316d9eac82eb0c1
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-03-18 13:56:28 +0100

    .ase files, ase cli export, dirt tiles

---
+++ android/assets/ase/export.bat
@@ -0,0 +1,10 @@
+@set ASEPRITE="D:\GAMES\STEAM\steamapps\common\Aseprite\aseprite.exe"
+@set OUT="../sprites/"
+
+@set ENTITYFORMAT="{path}/{title} #{tag} {frame00}.png"
+
+%ASEPRITE% -b biome_castle.ase --save-as %OUT%tiles-0.png 
+%ASEPRITE% -b items.ase --save-as %OUT%items.png 
+
+%ASEPRITE% -b actor_towelknight.ase --sheet %OUT%actor_towelknight.png 
+%ASEPRITE% -b actor_towelknight.ase --sheet-pack --sheet %OUT%actor_towelknight_packed.png 
