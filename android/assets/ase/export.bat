908a5f97d0c0f5eb42d454ab28a1ec0949b7e967
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-03-19 23:22:30 +0100

    split sprite export example

--- android/assets/ase/export.bat
+++ android/assets/ase/export.bat
@@ -4,7 +4,7 @@
 @set ENTITYFORMAT="{path}/{title} #{tag} {frame00}.png"
 
 %ASEPRITE% -b biome_castle.ase --save-as %OUT%tiles-0.png 
-%ASEPRITE% -b items.ase --save-as %OUT%items.png 
+%ASEPRITE% -b item.ase --save-as %OUT%item.png 
 
 %ASEPRITE% -b actor_towelknight.ase --sheet %OUT%actor_towelknight.png 
 %ASEPRITE% -b actor_towelknight.ase --sheet-pack --sheet %OUT%actor_towelknight_packed.png 
