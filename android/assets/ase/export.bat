aac5fa98f27a7377c9bd809ec79e272488471077
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-03-22 00:18:06 +0100

    biome 0 split export done

--- android/assets/ase/export.bat
+++ android/assets/ase/export.bat
@@ -1,10 +1,10 @@
 @set ASEPRITE="D:\GAMES\STEAM\steamapps\common\Aseprite\aseprite.exe"
-@set OUT="../sprites/"
+@set OUT="../sprites_split/"
+@set BFORMAT="{path}/{title} {layer}.{extension}"
 
-@set ENTITYFORMAT="{path}/{title} #{tag} {frame00}.png"
+%ASEPRITE% -b biome_castle.ase --split-layers --trim --save-as %OUT%biome-0.png
 
-%ASEPRITE% -b biome_castle.ase --save-as %OUT%tiles-0.png 
-%ASEPRITE% -b item.ase --save-as %OUT%item.png 
+%ASEPRITE% -b item.ase --split-layers --trim --save-as %OUT%item.png
 
-%ASEPRITE% -b actor_towelknight.ase --sheet %OUT%actor_towelknight.png 
-%ASEPRITE% -b actor_towelknight.ase --sheet-pack --sheet %OUT%actor_towelknight_packed.png 
+%ASEPRITE% -b actor_towelknight.ase --sheet %OUT%actor-towelknight.png --data %OUT%actor-towelknight-sheet.json
+%ASEPRITE% -b actor_towelknight.ase --split-tags --save-as %OUT%actor-towelknight-{tag}-{tagframe00}.png --data %OUT%actor-towelknight.json
