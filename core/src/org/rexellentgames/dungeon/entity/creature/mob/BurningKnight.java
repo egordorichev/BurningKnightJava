895fa1882236248f7d94b6482d5a5d85b71e33b4
Egor Dorichev <egordorichev@gmail.com>
2018-03-22 11:33:14 +0300

    Slowly moving to altases, the code won't work

--- core/src/org/rexellentgames/dungeon/entity/creature/mob/BurningKnight.java
+++ core/src/org/rexellentgames/dungeon/entity/creature/mob/BurningKnight.java
@@ -10,7 +10,6 @@ import org.rexellentgames.dungeon.entity.level.rooms.Room;
 import org.rexellentgames.dungeon.entity.level.rooms.regular.ladder.EntranceRoom;
 import org.rexellentgames.dungeon.entity.level.rooms.regular.ladder.ExitRoom;
 import org.rexellentgames.dungeon.util.Animation;
-import org.rexellentgames.dungeon.util.Log;
 import org.rexellentgames.dungeon.util.Random;
 import org.rexellentgames.dungeon.util.file.FileReader;
 import org.rexellentgames.dungeon.util.geometry.Point;
@@ -18,6 +17,26 @@ import org.rexellentgames.dungeon.util.geometry.Point;
 import java.io.IOException;
 
 public class BurningKnight extends Mob {
+	private static final int DASH_DIST = 64;
+	private static final int ATTACK_DIST = 32;
+	public static BurningKnight instance;
+	private static float LIGHT_SIZE = 8f;
+	private static Animation idle = Animation.make("actor-towelknight", "idle");
+	private static Animation hurt = Animation.make(0.1f, 32, 184, 186);
+	private static Animation killed = Animation.make(0.1f, 32, 188);
+	private Animation animation;
+	private float r;
+	private float g;
+	private float b;
+	private Player target;
+	private float idleTime;
+	private Point roomToVisit;
+	private Room last;
+	private float roamTime;
+	private Point lastTargetPosition = new Point();
+	private float dashWait;
+	private boolean[][] fx;
+
 	{
 		hpMax = 10000;
 		damage = 10;
@@ -27,15 +46,6 @@ public class BurningKnight extends Mob {
 		alwaysActive = true;
 		flying = true;
 	}
-
-	private static float LIGHT_SIZE = 8f;
-
-	public static BurningKnight instance;
-
-	private static Animation idle = Animation.make(Graphics.sprites, 0.08f, 32, 160, 162,
-		164, 166, 168, 170, 172, 174, 176, 178, 180, 182);
-	private static Animation hurt = Animation.make(Graphics.sprites, 0.1f, 32, 184, 186);
-	private static Animation killed = Animation.make(Graphics.sprites, 0.1f, 32, 188);
 
 	public void findStartPoint() {
 		Room room;
@@ -65,10 +75,6 @@ public class BurningKnight extends Mob {
 		this.body = this.createBody(8, 3, 16, 18, BodyDef.BodyType.DynamicBody, true);
 	}
 
-	private float r;
-	private float g;
-	private float b;
-
 	@Override
 	public void update(float dt) {
 		super.update(dt);
@@ -91,6 +97,10 @@ public class BurningKnight extends Mob {
 			return;
 		}
 
+		if (this.animation != null) {
+			this.animation.update(dt);
+		}
+
 		this.ai(dt);
 		super.common();
 	}
@@ -100,25 +110,20 @@ public class BurningKnight extends Mob {
 		this.vel.mul(0f);
 	}
 
-	private Player target;
-
 	@Override
 	public void render() {
 		Graphics.batch.setColor(1, 1, 1, this.a);
 
-		Animation animation;
-
 		if (this.dead) {
-			animation = killed;
+			this.animation = killed;
 		} else if (this.invt > 0) {
-			animation = hurt;
+			this.animation = hurt;
 		} else {
-			animation = idle;
+			this.animation = idle;
 		}
 
-		animation.render(this.x, this.y, this.t, this.flipped);
+		this.animation.render(this.x, this.y, this.flipped);
 	}
-
 
 	private void ai(float dt) {
 		if (this.state.equals("idle")) {
@@ -138,10 +143,10 @@ public class BurningKnight extends Mob {
 		}
 	}
 
-	private float idleTime;
-
 	private void idle(float dt) {
-		this.r = 0; this.g = 0; this.b = 0;
+		this.r = 0;
+		this.g = 0;
+		this.b = 0;
 
 		if (this.idleTime == 0) {
 			this.idleTime = Random.newFloat(5f, 10f);
@@ -152,10 +157,6 @@ public class BurningKnight extends Mob {
 
 		this.checkForTarget();
 	}
-
-	private Point roomToVisit;
-	private Room last;
-	private float roamTime;
 
 	private void checkForTarget() {
 		for (Player player : Player.all) {
@@ -178,7 +179,9 @@ public class BurningKnight extends Mob {
 	}
 
 	private void roam(float dt) {
-		this.r = 0; this.g = 0.0f; this.b = 0.3f;
+		this.r = 0;
+		this.g = 0.0f;
+		this.b = 0.3f;
 
 		if (this.roamTime == 0) {
 			this.roamTime = Random.newFloat(30f, 60f);
@@ -209,7 +212,7 @@ public class BurningKnight extends Mob {
 					room = Dungeon.level.getRandomRoom();
 					break;
 				}
-			} while(d > 400f && (this.last == null || last != room));
+			} while (d > 400f && (this.last == null || last != room));
 
 			this.roomToVisit = room.getCenter();
 			this.roomToVisit.mul(16);
@@ -236,15 +239,14 @@ public class BurningKnight extends Mob {
 	}
 
 	private void alerted(float dt) {
-		this.r = 0.8f; this.g = 0; this.b = 0.4f;
+		this.r = 0.8f;
+		this.g = 0;
+		this.b = 0.4f;
 
 		if (this.t >= 1f) {
 			this.become("chase");
 		}
 	}
-
-	private Point lastTargetPosition = new Point();
-	private float dashWait;
 
 	private void chase(float dt) {
 		float dx = 0;
@@ -263,7 +265,9 @@ public class BurningKnight extends Mob {
 
 		if (this.target == null || d > (this.target.getLightSize() + LIGHT_SIZE - 3) * 16) {
 			this.target = null;
-			this.r = 0.8f; this.g = 0.0f; this.b = 0.8f;
+			this.r = 0.8f;
+			this.g = 0.0f;
+			this.b = 0.8f;
 
 			dx = this.lastTargetPosition.x - this.x - 8;
 			dy = this.lastTargetPosition.y - this.y - 8;
@@ -275,7 +279,9 @@ public class BurningKnight extends Mob {
 				return;
 			}
 		} else {
-			this.r = 1; this.g = 0.2f; this.b = 0;
+			this.r = 1;
+			this.g = 0.2f;
+			this.b = 0;
 
 			this.lastTargetPosition.x = this.target.x;
 			this.lastTargetPosition.y = this.target.y;
@@ -296,9 +302,6 @@ public class BurningKnight extends Mob {
 		}
 	}
 
-	private static final int DASH_DIST = 64;
-	private static final int ATTACK_DIST = 32;
-
 	private void dash(float dt) {
 		float dx = 0;
 		float dy = 0;
@@ -312,7 +315,9 @@ public class BurningKnight extends Mob {
 
 		if (this.target == null || d > (this.target.getLightSize() + LIGHT_SIZE - 3) * 16) {
 			this.target = null;
-			this.r = 0.8f; this.g = 0.0f; this.b = 0.8f;
+			this.r = 0.8f;
+			this.g = 0.0f;
+			this.b = 0.8f;
 
 			dx = this.lastTargetPosition.x - this.x - 8;
 			dy = this.lastTargetPosition.y - this.y - 8;
@@ -323,7 +328,9 @@ public class BurningKnight extends Mob {
 				return;
 			}
 		} else {
-			this.r = 1; this.g = 0.2f; this.b = 0;
+			this.r = 1;
+			this.g = 0.2f;
+			this.b = 0;
 
 			this.lastTargetPosition.x = this.target.x;
 			this.lastTargetPosition.y = this.target.y;
@@ -344,17 +351,19 @@ public class BurningKnight extends Mob {
 	}
 
 	private void preattack(float dt) {
-		this.r = 1f; this.g = 0f; this.b = 0f;
+		this.r = 1f;
+		this.g = 0f;
+		this.b = 0f;
 
 		if (this.t > 0.5f) {
 			this.become("attack");
 		}
 	}
 
-	private boolean[][] fx;
-
 	private void attack(float dt) {
-		this.r = 0f; this.g = 1f; this.b = 0f;
+		this.r = 0f;
+		this.g = 1f;
+		this.b = 0f;
 
 		if (this.t % 0.5f <= 0.0175f) {
 			if (this.fx == null) {
