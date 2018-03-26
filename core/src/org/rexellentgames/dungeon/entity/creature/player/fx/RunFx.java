5a936d874aecd121aa469c3a3c4420643339c0e8
egordorichev <egordorichev@gmail.com>
2018-03-26 20:09:11 +0300

    Add run fx animation

--- core/src/org/rexellentgames/dungeon/entity/creature/player/fx/RunFx.java
+++ core/src/org/rexellentgames/dungeon/entity/creature/player/fx/RunFx.java
@@ -1,11 +1,10 @@
 package org.rexellentgames.dungeon.entity.creature.player.fx;
 
-import org.rexellentgames.dungeon.assets.Graphics;
 import org.rexellentgames.dungeon.entity.Entity;
 import org.rexellentgames.dungeon.util.Animation;
 
 public class RunFx extends Entity {
-	// private static Animation animation = new Animation(Graphics.effects, 0.05f, 16, 0, 1, 2, 3, 4, 5, 6);
+	private static Animation animation = Animation.make("run-fx", "idle");
 	private float t;
 
 	public RunFx(float x, float y) {
@@ -17,6 +16,7 @@ public class RunFx extends Entity {
 	@Override
 	public void update(float dt) {
 		this.t += dt;
+		this.animation.update(dt);
 
 		if (this.t >= 0.05f * 7) {
 			this.done = true;
@@ -25,6 +25,6 @@ public class RunFx extends Entity {
 
 	@Override
 	public void render() {
-		// this.animation.render(this.x, this.y + 8, this.t, false);
+		this.animation.render(this.x, this.y + 8, false);
 	}
 }
