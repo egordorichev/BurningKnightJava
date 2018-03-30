563753acc91288e9d7abedb44ba46dda3448641a
Egor Dorichev <egordorichev@gmail.com>
2018-03-30 15:03:16 +0300

    Clean up assets

--- core/src/org/rexellentgames/dungeon/assets/Packer.java
+++ core/src/org/rexellentgames/dungeon/assets/Packer.java
@@ -1,9 +1,29 @@
 package org.rexellentgames.dungeon.assets;
 
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import org.rexellentgames.dungeon.util.Log;
+
+import java.io.File;
+import java.io.FilenameFilter;
 
 public class Packer {
 	public final static void main(String[] args) {
+		final File folder = new File("sprites_split");
+
+		final File[] files = folder.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(final File dir, final String name) {
+				return name.indexOf('@') != -1;
+			}
+		});
+
+		for (final File file : files) {
+			Log.info("Delete " + file.getName());
+			if (!file.delete()) {
+				System.err.println("Can't remove " + file.getAbsolutePath());
+			}
+		}
+
 		TexturePacker.process("sprites_split", "atlas", "atlas");
 	}
 }
