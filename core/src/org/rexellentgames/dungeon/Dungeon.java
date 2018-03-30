5b2be12a8ada28e04b173b3836871bcf23c0d6e6
Egor Dorichev <egordorichev@gmail.com>
2018-03-30 11:52:19 +0300

    Fix solid placement bug

--- core/src/org/rexellentgames/dungeon/Dungeon.java
+++ core/src/org/rexellentgames/dungeon/Dungeon.java
@@ -132,6 +132,7 @@ public class Dungeon extends ApplicationAdapter {
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
 
 			if (Camera.instance != null) {
+				Camera.instance.applyShake();
 				Graphics.batch.setProjectionMatrix(Camera.instance.getCamera().combined);
 			}
 
@@ -144,6 +145,10 @@ public class Dungeon extends ApplicationAdapter {
 
 			area.render();
 			game.render();
+
+			if (Camera.instance != null) {
+				Camera.instance.removeShake();
+			}
 
 			Graphics.batch.end();
 
