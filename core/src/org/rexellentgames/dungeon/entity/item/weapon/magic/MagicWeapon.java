06394406e69f95567a9c6b647814249cce8ad9d9
Egor Dorichev <egordorichev@gmail.com>
2018-04-05 15:16:37 +0300

    Working on fire balls

---
+++ core/src/org/rexellentgames/dungeon/entity/item/weapon/magic/MagicWeapon.java
@@ -0,0 +1,59 @@
+package org.rexellentgames.dungeon.entity.item.weapon.magic;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import org.rexellentgames.dungeon.assets.Graphics;
+import org.rexellentgames.dungeon.entity.creature.Creature;
+import org.rexellentgames.dungeon.entity.creature.player.Player;
+import org.rexellentgames.dungeon.entity.item.Item;
+import org.rexellentgames.dungeon.game.input.Input;
+
+public class MagicWeapon extends Item {
+	protected int damage = 1;
+	protected float mana;
+	protected Player owner;
+
+	{
+		identified = true;
+	}
+
+	public void setOwner(Creature owner) {
+		this.owner = (Player) owner;
+	}
+
+	@Override
+	public void render(float x, float y, float w, float h, boolean flipped) {
+		if (this.delay > 0) {
+			float dx = Input.instance.worldMouse.x - this.owner.x - this.owner.w / 2;
+			float dy = Input.instance.worldMouse.y + this.owner.h / 2 - this.owner.y - this.owner.h / 2;
+			double a = Math.atan2(dy, dx);
+
+			TextureRegion s = this.getSprite();
+
+			Graphics.render(s, x, y, (float) a, s.getRegionWidth() / 2, s.getRegionHeight() / 2, false, false);
+		}
+	}
+
+	@Override
+	public void use() {
+		if (this.owner.getMana() < this.mana) {
+			return;
+		}
+
+		super.use();
+
+		this.owner.modifyMana(-this.mana);
+	}
+
+	@Override
+	public StringBuilder buildInfo() {
+		StringBuilder builder = super.buildInfo();
+
+		builder.append("Uses ");
+		builder.append((int) this.mana);
+		builder.append(" mana\n");
+		builder.append(this.damage);
+		builder.append(" damage\n");
+
+		return builder;
+	}
+}
