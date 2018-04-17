85cc46693a66e19fd7b3e41510b2fdc19d13b762
egordorichev <egordorichev@gmail.com>
2018-04-17 18:26:30 +0300

    Much better dialogs, implement key binding in json

--- core/src/org/rexellentgames/dungeon/util/DialogData.java
+++ core/src/org/rexellentgames/dungeon/util/DialogData.java
@@ -1,8 +1,6 @@
 package org.rexellentgames.dungeon.util;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.rafaskoberg.gdx.typinglabel.TypingLabel;
@@ -19,17 +17,34 @@ public class DialogData {
 	private static Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
 	private float delay = -1f;
 	private static TextureRegion frame = Graphics.getTexture("dialog");
+	private float a;
 
 	public void start() {
 		this.current = 0;
-		this.next();
+
+		Tween.to(new Tween.Task(1f, 0.3f) {
+			@Override
+			public float getValue() {
+				return a;
+			}
+
+			@Override
+			public void setValue(float value) {
+				a = value;
+			}
+
+			@Override
+			public void onEnd() {
+				next();
+			}
+		});
 	}
 
 	public void next() {
 		Dialog.Phrase phrase = this.phrases.get(this.current);
 		this.label = new TypingLabel("{SPEED=0.4}{COLOR=#FFFFFF}" + phrase.string, skin);
-		this.label.setSize(Display.GAME_WIDTH - 64, 64);
-		this.label.setPosition(32, Display.GAME_HEIGHT - 64 - 32);
+		this.label.setSize(Display.GAME_WIDTH - 96 - 16, 64);
+		this.label.setPosition(80, Display.GAME_HEIGHT - 64 - 32);
 		this.label.setWrap(true);
 
 		this.label.setTypingListener(new TypingListener() {
@@ -56,6 +71,10 @@ public class DialogData {
 	}
 
 	public void update(float dt) {
+		if (this.label == null) {
+			return;
+		}
+
 		this.label.act(dt);
 
 		if (this.delay != -1f) {
@@ -65,7 +84,22 @@ public class DialogData {
 				this.delay = -1f;
 
 				if (current == phrases.size() - 1) {
-					Dialog.active = null;
+					Tween.to(new Tween.Task(0f, 0.3f) {
+						@Override
+						public float getValue() {
+							return a;
+						}
+
+						@Override
+						public void setValue(float value) {
+							a = value;
+						}
+
+						@Override
+						public void onEnd() {
+							Dialog.active = null;
+						}
+					});
 				} else {
 					current += 1;
 					next();
@@ -75,7 +109,13 @@ public class DialogData {
 	}
 
 	public void render() {
+		Graphics.batch.setColor(1, 1, 1, this.a);
 		Graphics.render(frame, 16, Display.GAME_HEIGHT - 64 - 32);
-		this.label.draw(Graphics.batch, 1f);
+		Graphics.render(this.phrases.get(this.current).region, 32, Display.GAME_HEIGHT - 64 - 16);
+		Graphics.batch.setColor(1, 1, 1, 1);
+
+		if (this.label != null) {
+			this.label.draw(Graphics.batch, this.a);
+		}
 	}
 }
