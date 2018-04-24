82ac4d0c9817e26f2079285f1babc8a8ebe3fdfa
egordorichev <egordorichev@gmail.com>
2018-04-24 16:01:14 +0300

    More axes, improved their animation

--- core/src/org/rexellentgames/dungeon/entity/item/weapon/axe/Axe.java
+++ core/src/org/rexellentgames/dungeon/entity/item/weapon/axe/Axe.java
@@ -1,31 +1,106 @@
 package org.rexellentgames.dungeon.entity.item.weapon.axe;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import org.rexellentgames.dungeon.Dungeon;
+import org.rexellentgames.dungeon.assets.Graphics;
+import org.rexellentgames.dungeon.entity.creature.mob.Mob;
+import org.rexellentgames.dungeon.entity.creature.player.Player;
 import org.rexellentgames.dungeon.entity.item.weapon.Weapon;
+import org.rexellentgames.dungeon.game.input.Input;
+import org.rexellentgames.dungeon.util.Tween;
 
 public class Axe extends Weapon {
 	{
 		stackable = true;
 		penetrates = true;
+		ox = 0;
+		oy = 0;
+	}
+
+	private float added;
+	private float ox;
+	private float oy;
+
+	@Override
+	public void render(float x, float y, float w, float h, boolean flipped) {
+		if (this.added > 0) {
+			float angle = this.added;
+
+			if (this.owner != null) {
+				if (this.owner instanceof Player) {
+					float dx = this.owner.x + this.owner.w / 2 - Input.instance.worldMouse.x - 8;
+					float dy = this.owner.y + this.owner.h / 2 - Input.instance.worldMouse.y - 8;
+					float a = (float) Math.toDegrees(Math.atan2(dy, dx));
+
+					angle += (flipped ? a : -a);
+
+					angle = flipped ? angle : 180 - angle;
+				} else if (this.owner instanceof Mob && this.added == 0) {
+					Mob mob = (Mob) this.owner;
+
+					if (mob.target != null && mob.saw && !mob.isDead()) {
+						float dx = this.owner.x + this.owner.w / 2 - mob.target .x - mob.target.w / 2;
+						float dy = this.owner.y + this.owner.h / 2 - mob.target .y - mob.target.h / 2;
+						float a = (float) Math.toDegrees(Math.atan2(dy, dx));
+
+						angle += (flipped ? a : -a);
+					} else {
+						angle += (flipped ? 0 : 180);
+					}
+
+					angle = flipped ? angle : 180 - angle;
+				} else {
+					angle += (flipped ? 0 : 180);
+					angle = flipped ? angle : 180 - angle;
+				}
+			}
+
+			TextureRegion sprite = this.getSprite();
+
+			float xx = x + w / 2 + (flipped ? -w / 4 : w / 4);
+			float yy = y + (this.ox == 0 ? h / 4 : h / 2);
+
+			Graphics.render(sprite, xx - (flipped ? sprite.getRegionWidth() : 0), yy,
+				angle, sprite.getRegionWidth() / 2 + (flipped ? this.ox : -this.ox), this.oy, flipped, false);
+		}
 	}
 
 	@Override
 	public void use() {
 		super.use();
 
-		count -= 1;
+		Axe self = this;
 
-		AxeFx fx = new AxeFx();
+		Tween.to(new Tween.Task(90, 0.2f) {
+			@Override
+			public float getValue() {
+				return added;
+			}
 
-		fx.region = this.getSprite();
-		fx.type = this.getClass();
-		fx.x = this.owner.x + (this.owner.w - 16) / 2;
-		fx.y = this.owner.y + (this.owner.h - 16) / 2;
+			@Override
+			public void setValue(float value) {
+				added = value;
+			}
 
-		fx.owner = this.owner;
-		fx.damage = this.damage;
-		fx.penetrates = this.penetrates;
+			@Override
+			public void onEnd() {
+				count -= 1;
+				added = 0;
 
-		Dungeon.area.add(fx);
+				AxeFx fx = new AxeFx();
+
+				fx.region = getSprite();
+				fx.type = self.getClass();
+				fx.x = owner.x + (owner.w - 16) / 2;
+				fx.y = owner.y + (owner.h - 16) / 2;
+
+				fx.owner = owner;
+				fx.damage = damage;
+				fx.penetrates = penetrates;
+
+				Dungeon.area.add(fx);
+				endUse();
+			}
+		});
 	}
 }
---
+++ core/src/org/rexellentgames/dungeon/entity/item/weapon/axe/AxeC.java
@@ -0,0 +1,13 @@
+package org.rexellentgames.dungeon.entity.item.weapon.axe;
+
+import org.rexellentgames.dungeon.assets.Locale;
+
+public class AxeC extends Axe {
+	{
+		name = Locale.get("axe_c");
+		description = Locale.get("axe_c_desc");
+		damage = 11;
+		penetrates = true;
+		sprite = "item (axe C)";
+	}
+}
