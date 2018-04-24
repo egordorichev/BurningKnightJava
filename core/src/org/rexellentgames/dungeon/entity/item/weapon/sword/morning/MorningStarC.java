0d92b8b2ca2d4884e61081187d898afdbd76ae8e
egordorichev <egordorichev@gmail.com>
2018-04-24 13:04:15 +0300

    Morning stars and some tiny fixes

---
+++ core/src/org/rexellentgames/dungeon/entity/item/weapon/sword/morning/MorningStarC.java
@@ -0,0 +1,13 @@
+package org.rexellentgames.dungeon.entity.item.weapon.sword.morning;
+
+import org.rexellentgames.dungeon.assets.Locale;
+
+public class MorningStarC extends MorningStar {
+	{
+		sprite = "item (morning C)";
+		name = Locale.get("morning_c");
+		description = Locale.get("morning_c_desc");
+		damage = 10;
+		minDamage = 10;
+	}
+}
