2979bf544c9d25d24d0b3ffc45091d2c948ffeac
egordorichev <egordorichev@gmail.com>
2018-04-26 19:54:33 +0300

    Fixes

--- core/src/org/rexellentgames/dungeon/entity/item/weapon/ranged/WoodenBow.java
+++ core/src/org/rexellentgames/dungeon/entity/item/weapon/ranged/WoodenBow.java
@@ -5,7 +5,7 @@ import org.rexellentgames.dungeon.assets.Locale;
 public class WoodenBow extends Bow {
 	{
 		name = Locale.get("bow");
-		sprite = "item (bow B)";
+		sprite = "item (bow A)";
 		description = Locale.get("bow_desc");
 		useTime = 0.4f;
 		damage = 6;
