1d3e4faf42c7a86031deccc5337069cd689dad85
egordorichev <egordorichev@gmail.com>
2018-05-01 18:00:19 +0300

    Fully working graphics settings (saving also works)

--- core/src/org/rexellentgames/dungeon/Settings.java
+++ core/src/org/rexellentgames/dungeon/Settings.java
@@ -1,5 +1,15 @@
 package org.rexellentgames.dungeon;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.JsonWriter;
+import org.rexellentgames.dungeon.util.Log;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Settings {
 	public static boolean fullscreen;
 	public static boolean vsync = true;
@@ -7,4 +17,40 @@ public class Settings {
 	public static boolean gore = true;
 	public static boolean shaders = false;
 	public static int quality = 2;
+	public static float screenshake = 0.7f;
+
+	public static void load() {
+		Log.info("Loading settings...");
+
+		FileHandle handle = Gdx.files.external(".bk/settings.json");
+
+		if (handle.exists()) {
+			JsonReader reader = new JsonReader();
+			JsonValue root = reader.parse(handle);
+
+			fullscreen = root.getBoolean("fullscreen");
+			blood = root.getBoolean("blood");
+			gore = root.getBoolean("gore");
+			shaders = root.getBoolean("shaders");
+			vsync = root.getBoolean("vsync");
+			quality = root.getInt("quality");
+			screenshake = root.getFloat("screenshake");
+		}
+	}
+
+	public static void save() {
+		Log.info("Saving settings...");
+
+		try {
+			FileWriter w = new FileWriter(Gdx.files.external(".bk/settings.json").file().getAbsolutePath());
+			JsonWriter writer = new JsonWriter(w);
+
+			writer.write("{ \"fullscreen\" : " + fullscreen + ", \"blood\" : " + blood + ", \"gore\" : " + gore +
+				", \"shaders\" : " + shaders + ", \"vsync\" : " + vsync + ", \"quality\" : " + quality + ", \"screenshake\" : " + ((double) screenshake) + " }");
+
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
