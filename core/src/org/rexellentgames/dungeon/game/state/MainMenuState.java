cbfd11fcf0cf32c0fd80cfb3b65e753040450698
egordorichev <egordorichev@gmail.com>
2018-05-02 20:10:40 +0300

    Added simple logo

--- core/src/org/rexellentgames/dungeon/game/state/MainMenuState.java
+++ core/src/org/rexellentgames/dungeon/game/state/MainMenuState.java
@@ -2,6 +2,7 @@ package org.rexellentgames.dungeon.game.state;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import org.rexellentgames.dungeon.Display;
 import org.rexellentgames.dungeon.Dungeon;
@@ -12,6 +13,8 @@ import org.rexellentgames.dungeon.ui.UiButton;
 import org.rexellentgames.dungeon.util.Tween;
 
 public class MainMenuState extends State {
+	private static TextureRegion logo = Graphics.getTexture("logo");
+
 	@Override
 	public void init() {
 		Dungeon.area.add(Camera.instance);
@@ -64,6 +67,24 @@ public class MainMenuState extends State {
 	@Override
 	public void renderUi() {
 		super.renderUi();
+
+		float sx = (float) (1f + Math.sin(Dungeon.time / 1.5f) / 5);
+		float sy = (float) (1f + Math.cos(Dungeon.time) / 5);
+		float a = (float) (Math.cos(Dungeon.time * 4) * 3f);
+
+		Graphics.batch.setColor(UiButton.outline.r, UiButton.outline.g, UiButton.outline.b, 1);
+
+		for (int xx = -1; xx < 2; xx++) {
+			for (int yy = -1; yy < 2; yy++) {
+				Graphics.render(logo, Display.GAME_WIDTH / 2 + xx, 200 + yy,
+					a,
+					logo.getRegionWidth() / 2, logo.getRegionHeight() / 2, false, false, sx, sy);
+			}
+		}
+
+		Graphics.batch.setColor(1, 1, 1, 1);
+
+		Graphics.render(logo, Display.GAME_WIDTH / 2, 200, a, logo.getRegionWidth() / 2, logo.getRegionHeight() / 2, false, false, sx, sy);
 
 		Ui.ui.renderCursor();
 	}
