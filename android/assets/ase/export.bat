b88e8e24f63596cefe98cbbf99cce01cf698f4dc
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-05-04 21:12:32 +0200

    gobbo armors

--- android/assets/ase/export.bat
+++ android/assets/ase/export.bat
@@ -7,7 +7,6 @@
 %ASEPRITE% -b item.ase --trim --save-as %OUT%item.png
 %ASEPRITE% -b veggie.ase --crop 16,16,16,16 --split-layers --split-tags --list-tags --save-as %OUT%veggie-{layer}-{tag}-{tagframe00}.png --data %OUT%veggie.json
 
-%ASEPRITE% -b actor_gobbo.ase --trim --list-tags --save-as %OUT%actor-gobbo-{tag}-{tagframe00}.png --data %OUT%actor-gobbo.json
 %ASEPRITE% -b actor_burning_knight.ase --trim --list-tags --save-as %OUT%actor-burning-knight-{tag}-{tagframe00}.png --data %OUT%actor-burning-knight.json
 %ASEPRITE% -b actor_towelknight.ase --trim --list-tags --save-as %OUT%actor-towelknight-{tag}-{tagframe00}.png --data %OUT%actor-towelknight.json
 %ASEPRITE% -b actor_towelking.ase --trim --list-tags --save-as %OUT%actor-towelking-{tag}-{tagframe00}.png --data %OUT%actor-towelking.json
