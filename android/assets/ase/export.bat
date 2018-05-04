7b5ebb6a297932ecbc37a5ddce60b348223fdcc2
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-05-04 07:46:42 +0200

    trimmed everything

--- android/assets/ase/export.bat
+++ android/assets/ase/export.bat
@@ -4,24 +4,24 @@
 
 %ASEPRITE% -b biome_castle.ase --split-layers --trim --save-as %OUT%biome-0.png
 
-%ASEPRITE% -b item.ase --split-layers --trim --save-as %OUT%item.png
-%ASEPRITE% -b veggie.ase  --crop 16,16,16,16 --split-layers --split-tags --list-tags --save-as %OUT%veggie-{layer}-{tag}-{tagframe00}.png --data %OUT%veggie.json
+%ASEPRITE% -b item.ase --trim --save-as %OUT%item.png
+%ASEPRITE% -b veggie.ase --crop 16,16,16,16 --split-layers --split-tags --list-tags --save-as %OUT%veggie-{layer}-{tag}-{tagframe00}.png --data %OUT%veggie.json
 
-%ASEPRITE% -b actor_gobbo.ase --split-tags --list-tags --save-as %OUT%actor-gobbo-{tag}-{tagframe00}.png --data %OUT%actor-gobbo.json
+%ASEPRITE% -b actor_gobbo.ase --trim --list-tags --save-as %OUT%actor-gobbo-{tag}-{tagframe00}.png --data %OUT%actor-gobbo.json
 %ASEPRITE% -b actor_burning_knight.ase --trim --list-tags --save-as %OUT%actor-burning-knight-{tag}-{tagframe00}.png --data %OUT%actor-burning-knight.json
-%ASEPRITE% -b actor_towelknight.ase --list-tags --save-as %OUT%actor-towelknight-{tag}-{tagframe00}.png --data %OUT%actor-towelknight.json
+%ASEPRITE% -b actor_towelknight.ase --trim --list-tags --save-as %OUT%actor-towelknight-{tag}-{tagframe00}.png --data %OUT%actor-towelknight.json
 %ASEPRITE% -b actor_towelking.ase --trim --list-tags --save-as %OUT%actor-towelking-{tag}-{tagframe00}.png --data %OUT%actor-towelking.json
-%ASEPRITE% -b actor_clown.ase --list-tags --save-as %OUT%actor-clown-{tag}-{tagframe00}.png --data %OUT%actor-clown.json
+%ASEPRITE% -b actor_clown.ase --trim --list-tags --save-as %OUT%actor-clown-{tag}-{tagframe00}.png --data %OUT%actor-clown.json
 
-%ASEPRITE% -b actor_door_horizontal.ase --list-tags --save-as %OUT%actor_door_horizontal-{tag}-{tagframe00}.png --data %OUT%actor_door_horizontal.json
-%ASEPRITE% -b actor_door_vertical.ase --list-tags --save-as %OUT%actor_door_vertical-{tag}-{tagframe00}.png --data %OUT%actor_door_vertical.json
+%ASEPRITE% -b actor_door_horizontal.ase --trim --list-tags --save-as %OUT%actor_door_horizontal-{tag}-{tagframe00}.png --data %OUT%actor_door_horizontal.json
+%ASEPRITE% -b actor_door_vertical.ase --trim --list-tags --save-as %OUT%actor_door_vertical-{tag}-{tagframe00}.png --data %OUT%actor_door_vertical.json
 
 %ASEPRITE% -b prop_throne.ase --trim --list-tags --save-as %OUT%prop-throne-{layer}-{tag}-{tagframe00}.png --data %OUT%prop-throne.json
 
-%ASEPRITE% -b fx_plant.ase --list-tags --save-as %OUT%fx-plant-{tag}-{tagframe00}.png --data %OUT%fx-plant.json
-%ASEPRITE% -b fx_run.ase --list-tags --save-as %OUT%fx-run-{tag}-{tagframe00}.png --data %OUT%fx-run.json
-%ASEPRITE% -b fx_sword.ase --list-tags --save-as %OUT%fx-sword-{tag}-{tagframe00}.png --data %OUT%fx-sword.json
-%ASEPRITE% -b fx_dither.ase --list-tags --save-as %OUT%fx-dither-{tag}-{tagframe00}.png --data %OUT%fx-dither.json
-%ASEPRITE% -b fx_fireball.ase --list-tags --save-as %OUT%fx-fireball-{tag}-{tagframe00}.png --data %OUT%fx-fireball.json
+%ASEPRITE% -b fx_plant.ase --trim --list-tags --save-as %OUT%fx-plant-{tag}-{tagframe00}.png --data %OUT%fx-plant.json
+%ASEPRITE% -b fx_run.ase --trim --list-tags --save-as %OUT%fx-run-{tag}-{tagframe00}.png --data %OUT%fx-run.json
+%ASEPRITE% -b fx_sword.ase --trim --list-tags --save-as %OUT%fx-sword-{tag}-{tagframe00}.png --data %OUT%fx-sword.json
+%ASEPRITE% -b fx_dither.ase --trim --list-tags --save-as %OUT%fx-dither-{tag}-{tagframe00}.png --data %OUT%fx-dither.json
+%ASEPRITE% -b fx_fireball.ase --trim --list-tags --save-as %OUT%fx-fireball-{tag}-{tagframe00}.png --data %OUT%fx-fireball.json
 
 %ASEPRITE% -b bk_bars.ase --trim --save-as %OUT%bk_{layer}.png
