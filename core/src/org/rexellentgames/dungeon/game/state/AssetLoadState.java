765792605bf9ecf17f9c6052f04fb0f977d65691
egordorichev <egordorichev@gmail.com>
2018-05-07 16:34:09 +0300

    v0.0.10.6

--- core/src/org/rexellentgames/dungeon/game/state/AssetLoadState.java
+++ core/src/org/rexellentgames/dungeon/game/state/AssetLoadState.java
@@ -13,10 +13,9 @@ public class AssetLoadState extends State {
 			if (!Network.NONE) {
 				Dungeon.game.setState(new LoginState());
 			} else {
-				//Dungeon.game.setState(new MainMenuState());
-				//Dungeon.game.setState(new KeyConfigState());
-				LoadState.readDepth();
-			  Dungeon.goToLevel(Dungeon.depth);
+				Dungeon.game.setState(new MainMenuState());
+				//LoadState.readDepth();
+			  //Dungeon.goToLevel(Dungeon.depth);
 			}
 		}
 	}
