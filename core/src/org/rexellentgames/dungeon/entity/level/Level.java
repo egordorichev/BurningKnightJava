20bf7ab3b3191772ee2e30c39b18f3fec1b013db
Egor Dorichev <egordorichev@gmail.com>
2018-05-07 15:15:07 +0300

    Slowly removing BK rage

--- core/src/org/rexellentgames/dungeon/entity/level/Level.java
+++ core/src/org/rexellentgames/dungeon/entity/level/Level.java
@@ -390,7 +390,11 @@ public abstract class Level extends Entity {
 			float v = this.light[i];
 
 			if (v > 0) {
-				this.light[i] = MathUtils.clamp(Dungeon.level.addLight && (BurningKnight.instance == null) ? 1f : 0, 1f, v - dt * 2);
+				int x = i % getWidth();
+				int y = (int) Math.floor(i / getWidth());
+				
+				this.light[i] = MathUtils.clamp(Dungeon.level.addLight && (BurningKnight.instance == null) ? 1f :
+					((Dungeon.level.checkFor(x, y, Terrain.PASSABLE) || Dungeon.level.checkFor(x, y - 1, Terrain.PASSABLE)) ? 0.4f : 0), 1f, v - dt * 2);
 				this.lightR[i] = MathUtils.clamp(LIGHT_R, 1f, this.lightR[i] - dt);
 				this.lightG[i] = MathUtils.clamp(LIGHT_G, 1f, this.lightG[i] - dt);
 				this.lightB[i] = MathUtils.clamp(LIGHT_B, 1f, this.lightB[i] - dt);
