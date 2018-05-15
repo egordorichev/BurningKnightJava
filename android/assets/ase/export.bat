a36ba92a816df9c7383103d3ffa964da7a9ef5bb
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-05-15 22:55:07 +0200

    lava, props, bk fireball recolor, blood recolor

--- android/assets/ase/export.bat
+++ android/assets/ase/export.bat
@@ -16,11 +16,3 @@
 %ASEPRITE% -b actor_door_vertical.ase --trim --list-tags --save-as %OUT%actor_door_vertical-{tag}-{tagframe00}.png --data %OUT%actor_door_vertical.json
 
 %ASEPRITE% -b prop_throne.ase --trim --list-tags --save-as %OUT%prop-throne-{layer}-{tag}-{tagframe00}.png --data %OUT%prop-throne.json
-
-%ASEPRITE% -b fx_plant.ase --trim --list-tags --save-as %OUT%fx-plant-{tag}-{tagframe00}.png --data %OUT%fx-plant.json
-%ASEPRITE% -b fx_run.ase --trim --list-tags --save-as %OUT%fx-run-{tag}-{tagframe00}.png --data %OUT%fx-run.json
-%ASEPRITE% -b fx_sword.ase --trim --list-tags --save-as %OUT%fx-sword-{tag}-{tagframe00}.png --data %OUT%fx-sword.json
-%ASEPRITE% -b fx_dither.ase --trim --list-tags --save-as %OUT%fx-dither-{tag}-{tagframe00}.png --data %OUT%fx-dither.json
-%ASEPRITE% -b fx_fireball.ase --trim --list-tags --save-as %OUT%fx-fireball-{tag}-{tagframe00}.png --data %OUT%fx-fireball.json
-
-%ASEPRITE% -b bk_bars.ase --trim --save-as %OUT%bk_{layer}.png
