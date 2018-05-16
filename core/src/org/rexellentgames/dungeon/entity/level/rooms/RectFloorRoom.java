15a797980feb2d42b0f6f009286ce7e34dafdbab
egordorichev <egordorichev@gmail.com>
2018-05-16 15:48:38 +0300

    Another random decor room

---
+++ core/src/org/rexellentgames/dungeon/entity/level/rooms/RectFloorRoom.java
@@ -0,0 +1,17 @@
+package org.rexellentgames.dungeon.entity.level.rooms;
+
+import org.rexellentgames.dungeon.entity.level.Level;
+import org.rexellentgames.dungeon.entity.level.Terrain;
+import org.rexellentgames.dungeon.entity.level.painters.Painter;
+import org.rexellentgames.dungeon.entity.level.rooms.regular.RegularRoom;
+
+public class RectFloorRoom extends RegularRoom {
+	@Override
+	public void paint(Level level) {
+		super.paint(level);
+
+		for (int i = 1; i < this.getWidth() / 2; i++) {
+			Painter.fill(level, this, i, i % 2 == 0 ? Terrain.WOOD : Terrain.FLOOR);
+		}
+	}
+}
--- core/src/org/rexellentgames/dungeon/entity/level/rooms/regular/RegularRoom.java
+++ core/src/org/rexellentgames/dungeon/entity/level/rooms/regular/RegularRoom.java
@@ -5,6 +5,7 @@ import org.rexellentgames.dungeon.entity.level.Level;
 import org.rexellentgames.dungeon.entity.level.Terrain;
 import org.rexellentgames.dungeon.entity.level.features.Door;
 import org.rexellentgames.dungeon.entity.level.painters.Painter;
+import org.rexellentgames.dungeon.entity.level.rooms.RectFloorRoom;
 import org.rexellentgames.dungeon.entity.level.rooms.Room;
 import org.rexellentgames.dungeon.util.Random;
 import org.rexellentgames.dungeon.util.geometry.Rect;
@@ -37,16 +38,6 @@ public class RegularRoom extends Room {
 	public void paint(Level level) {
 		Painter.fill(level, this, Terrain.WALL);
 		Painter.fill(level, this, 1, Terrain.FLOOR);
-
-		if (Random.chance(30) && this.getWidth() > 7 && this.getHeight() > 7) {
-			Rect rect = new Rect();
-
-			rect.resize(Random.newInt(2, this.getWidth() - 3), Random.newInt(2, this.getHeight() - 3));
-			rect.setPos(Random.newInt(2, this.getWidth() - rect.getWidth() - 2) + this.left,
-				Random.newInt(2, this.getHeight() - rect.getHeight() - 2) + this.top);
-
-			// Painter.fill(level, rect, Terrain.TABLE);
-		}
 
 		for (Door door : this.connected.values()) {
 			door.setType(Door.Type.REGULAR);
@@ -92,13 +83,13 @@ public class RegularRoom extends Room {
 	private static ArrayList<Class<? extends RegularRoom>> rooms = new ArrayList<Class<? extends RegularRoom>>(Arrays.asList(
 		RegularRoom.class, GardenRoom.class, FloodedRoom.class, SpikedRoom.class,
 		MazeRoom.class, MazeFloorRoom.class, ChestTrapRoom.class, StatueRoom.class,
-		TableRoom.class, CenterTableRoom.class, CaveRoom.class
+		TableRoom.class, CenterTableRoom.class, CaveRoom.class, RectFloorRoom.class
 	));
 
 	private static float[] chances = new float[] {
 		5, 3, 1, 1,
 		0.05f, 1, 0.3f, 1,
-		1, 1, 4
+		1, 1, 2, 1000
 	};
 
 	public static RegularRoom create() {
