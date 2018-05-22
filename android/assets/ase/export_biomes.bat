c67a4aca3d9ea2af456e5ccc4af58935dd6c55bf
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-05-23 00:05:06 +0200

    added generic biome ase (pool mask, pool edge, wall edge, patterns), add new biome template (biome-0) and assoicated exports.

--- android/assets/ase/export_biomes.bat
+++ android/assets/ase/export_biomes.bat
@@ -2,3 +2,6 @@
 @set OUT="../sprites_split/"
 
 %ASEPRITE% -b biome_castle.ase --save-as %OUT%biome-0-{slice}.png
+
+%ASEPRITE% -b biome_gen.ase --save-as %OUT%biome-gen-{slice}.png
+%ASEPRITE% -b biome_0.ase --save-as %OUT%biome-0-{slice}.png
