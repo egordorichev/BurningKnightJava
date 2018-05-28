a1aabee9841ec34de73587385a6e417c24233754
egordorichev <egordorichev@gmail.com>
2018-05-28 21:05:10 +0300

    Saving chair flip

--- core/src/org/rexellentgames/dungeon/entity/level/entities/Chair.java
+++ core/src/org/rexellentgames/dungeon/entity/level/entities/Chair.java
@@ -1,5 +1,12 @@
 package org.rexellentgames.dungeon.entity.level.entities;
 
+import org.rexellentgames.dungeon.assets.Graphics;
+import org.rexellentgames.dungeon.util.file.FileReader;
+import org.rexellentgames.dungeon.util.file.FileWriter;
+import org.rexellentgames.dungeon.util.path.Graph;
+
+import java.io.IOException;
+
 public class Chair extends Prop {
 	public boolean flipped;
 
@@ -7,5 +14,21 @@ public class Chair extends Prop {
 	public void init() {
 		this.sprite = flipped ? "biome-0 (chair A)" : "biome-0 (chair B)";
 		super.init();
+	}
+
+	@Override
+	public void load(FileReader reader) throws IOException {
+		super.load(reader);
+
+		this.sprite = flipped ? "biome-0 (chair A)" : "biome-0 (chair B)";
+		region = Graphics.getTexture(this.sprite);
+		flipped = reader.readBoolean();
+	}
+
+	@Override
+	public void save(FileWriter writer) throws IOException {
+		super.save(writer);
+
+		writer.writeBoolean(flipped);
 	}
 }
