335396423b57844a64399e1cff95c4b6339dbe24
egordorichev <egordorichev@gmail.com>
2018-05-28 18:20:51 +0300

    Secret room with gold

--- core/src/org/rexellentgames/dungeon/entity/pool/room/SecretRoomPool.java
+++ core/src/org/rexellentgames/dungeon/entity/pool/room/SecretRoomPool.java
@@ -2,6 +2,7 @@ package org.rexellentgames.dungeon.entity.pool.room;
 
 import org.rexellentgames.dungeon.entity.level.rooms.secret.BombRoom;
 import org.rexellentgames.dungeon.entity.level.rooms.secret.ChestRoom;
+import org.rexellentgames.dungeon.entity.level.rooms.secret.GoldSecretRoom;
 import org.rexellentgames.dungeon.entity.level.rooms.secret.SecretRoom;
 import org.rexellentgames.dungeon.entity.pool.Pool;
 
@@ -10,6 +11,7 @@ public class SecretRoomPool extends Pool<SecretRoom> {
 
 	public SecretRoomPool() {
 		add(BombRoom.class, 1f);
-		add(ChestRoom.class, 100f);
+		add(ChestRoom.class, 1f);
+		add(GoldSecretRoom.class, 100f);
 	}
 }
