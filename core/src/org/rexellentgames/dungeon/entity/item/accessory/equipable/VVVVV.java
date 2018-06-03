86dd4d53b691f70976fab02b3f21d8a384f06d6d
egordorichev <egordorichev@gmail.com>
2018-06-03 20:03:29 +0300

    VVVVV localisation

--- core/src/org/rexellentgames/dungeon/entity/item/accessory/equipable/VVVVV.java
+++ core/src/org/rexellentgames/dungeon/entity/item/accessory/equipable/VVVVV.java
@@ -1,13 +1,24 @@
 package org.rexellentgames.dungeon.entity.item.accessory.equipable;
 
 import org.rexellentgames.dungeon.Dungeon;
+import org.rexellentgames.dungeon.assets.Locale;
+import org.rexellentgames.dungeon.entity.creature.player.Player;
 
 public class VVVVV extends Equipable {
+	{
+		name = Locale.get("vvvvv");
+		description = Locale.get("vvvvv_desc");
+	}
+
 	@Override
 	public void onEquip() {
 		super.onEquip();
 
 		Dungeon.flip = !Dungeon.flip;
+
+		if (this.owner instanceof Player) {
+			((Player) this.owner).goldModifier += 30f;
+		}
 	}
 
 	@Override
@@ -15,5 +26,9 @@ public class VVVVV extends Equipable {
 		super.onEquip();
 
 		Dungeon.flip = !Dungeon.flip;
+
+		if (this.owner instanceof Player) {
+			((Player) this.owner).goldModifier -= 30f;
+		}
 	}
 }
