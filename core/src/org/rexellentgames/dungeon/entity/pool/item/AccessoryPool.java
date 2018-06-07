e172d3c024a9a3953a1193f5491f9171d1fcc9ea
egordorichev <egordorichev@gmail.com>
2018-06-07 09:03:15 +0300

    Updated pool

--- core/src/org/rexellentgames/dungeon/entity/pool/item/AccessoryPool.java
+++ core/src/org/rexellentgames/dungeon/entity/pool/item/AccessoryPool.java
@@ -3,6 +3,9 @@ package org.rexellentgames.dungeon.entity.pool.item;
 import org.rexellentgames.dungeon.entity.item.Item;
 import org.rexellentgames.dungeon.entity.item.accessory.equipable.*;
 import org.rexellentgames.dungeon.entity.item.pet.Bumbo;
+import org.rexellentgames.dungeon.entity.item.pet.LibGDX;
+import org.rexellentgames.dungeon.entity.item.pet.Pico8;
+import org.rexellentgames.dungeon.entity.item.pet.StrawberryPet;
 import org.rexellentgames.dungeon.entity.item.pet.orbital.GooOrbital;
 import org.rexellentgames.dungeon.entity.item.reference.Dendy;
 import org.rexellentgames.dungeon.entity.item.reference.MagicMushroom;
@@ -15,6 +18,9 @@ public class AccessoryPool extends Pool<Item> {
 
 	public AccessoryPool() {
 		add(FireFlower.class, 0.3f);
+		add(StrawberryPet.class, 0.1f);
+		add(Pico8.class, 0.1f);
+		add(LibGDX.class, 0.1f);
 		add(FireRing.class, 1f);
 		add(IceRing.class, 1f);
 		add(ThornRing.class, 1f);
