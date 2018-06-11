cff80dd45a96ec478f7157a1c1b3dd6b7ec839f0
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-06-11 22:43:36 +0200

    converted .ase and assets of biome-14, items, props, BK, veggies to endesga64

--- android/assets/ase/export_biomes.bat
+++ android/assets/ase/export_biomes.bat
@@ -4,5 +4,3 @@
 %ASEPRITE% -b biome_gen.ase --save-as %OUT%biome-gen-{slice}.png
 %ASEPRITE% -b biome_0.ase --save-as %OUT%biome-0-{slice}.png
 %ASEPRITE% -b biome_1.ase --save-as %OUT%biome-1-{slice}.png
-
-%ASEPRITE% -b biome_3.ase --save-as %OUT%biome-3-{slice}.png
