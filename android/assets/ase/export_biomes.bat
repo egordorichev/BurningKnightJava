85f12bed4756ecd6f0d90dac6bc73ece94540a9e
mito mac / czm <mate.cziner@gmail.com>
2018-06-18 13:13:18 +0200

    turned off bg in biome-3, added line to export script

--- android/assets/ase/export_biomes.bat
+++ android/assets/ase/export_biomes.bat
@@ -5,3 +5,4 @@
 %ASEPRITE% -b biome_0.ase --save-as %OUT%biome-0-{slice}.png
 %ASEPRITE% -b biome_1.ase --save-as %OUT%biome-1-{slice}.png
 %ASEPRITE% -b biome_2.ase --save-as %OUT%biome-2-{slice}.png
+%ASEPRITE% -b biome_3.ase --save-as %OUT%biome-3-{slice}.png
