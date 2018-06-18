0b4983ba2a3c256fcb98c677463b216bae4d9373
egordorichev <egordorichev@gmail.com>
2018-06-18 17:23:56 +0300

    Reexported biome 4

--- core/src/org/rexcellentgames/burningknight/entity/pool/MobPool.java
+++ core/src/org/rexcellentgames/burningknight/entity/pool/MobPool.java
@@ -4,6 +4,7 @@ import org.rexcellentgames.burningknight.Dungeon;
 import org.rexcellentgames.burningknight.entity.creature.mob.Mob;
 import org.rexcellentgames.burningknight.entity.creature.mob.desert.*;
 import org.rexcellentgames.burningknight.entity.creature.mob.hall.*;
+import org.rexcellentgames.burningknight.entity.level.levels.creep.CreepLevel;
 import org.rexcellentgames.burningknight.entity.level.levels.desert.DesertLevel;
 import org.rexcellentgames.burningknight.entity.level.levels.hall.HallLevel;
 import org.rexcellentgames.burningknight.entity.level.levels.library.LibraryLevel;
@@ -45,6 +46,11 @@ public class MobPool extends Pool<Mob> {
 		}
 
 		if (Dungeon.level instanceof TechLevel) {
+			add(Skeleton.class, 1f);
+			// tmp
+		}
+
+		if (Dungeon.level instanceof CreepLevel) {
 			add(Skeleton.class, 1f);
 			// tmp
 		}
