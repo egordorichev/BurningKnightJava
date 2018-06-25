617899b6b51a39baf004e14b21676ff9dbb26b20
CZM-DELL17R5737\Mate <mate.cziner@gmail.com>
2018-06-25 22:39:50 +0200

    biome-6 (meat), no asset exported, but added new biomes to export script

--- core/assets/ase/export_biomes.bat
+++ core/assets/ase/export_biomes.bat
@@ -6,3 +6,6 @@
 %ASEPRITE% -b biome_1.ase --save-as %OUT%biome-1-{slice}.png
 %ASEPRITE% -b biome_2.ase --save-as %OUT%biome-2-{slice}.png
 %ASEPRITE% -b biome_3.ase --save-as %OUT%biome-3-{slice}.png
+%ASEPRITE% -b biome_4.ase --save-as %OUT%biome-4-{slice}.png
+%ASEPRITE% -b biome_5.ase --save-as %OUT%biome-5-{slice}.png
+%ASEPRITE% -b biome_6.ase --save-as %OUT%biome-6-{slice}.png
