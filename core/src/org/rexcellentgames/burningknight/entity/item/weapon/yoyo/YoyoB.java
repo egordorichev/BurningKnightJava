dd8b3ad3559306a3eb129a87d817bff6c8f48ed5
Egor Dorichev <egordorichev@gmail.com>
2018-07-11 10:22:19 +0300

    Fixes/updates to yoyos

--- core/src/org/rexcellentgames/burningknight/entity/item/weapon/yoyo/YoyoB.java
+++ core/src/org/rexcellentgames/burningknight/entity/item/weapon/yoyo/YoyoB.java
@@ -1,12 +1,14 @@
 package org.rexcellentgames.burningknight.entity.item.weapon.yoyo;
 
+import org.rexcellentgames.burningknight.assets.Graphics;
 import org.rexcellentgames.burningknight.assets.Locale;
 
 public class YoyoB extends Yoyo {
 	{
 		name = Locale.get("yoyo_b");
 		description = Locale.get("yoyo_b_desc");
-		sprite = "item-yoyo B";
+		sprite = "item-yoyo_icon B";
+		projectile = Graphics.getTexture("item-yoyo B");
 		damage = 9;
 		max = 150;
 	}
