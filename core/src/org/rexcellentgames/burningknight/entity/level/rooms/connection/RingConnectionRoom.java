8ec20c4553e7214c369d0bf7499da9d4307d00bb
Egor Dorichev <egordorichev@gmail.com>
2018-07-14 12:04:50 +0300

    Fixes to ring connection room

--- core/src/org/rexcellentgames/burningknight/entity/level/rooms/connection/RingConnectionRoom.java
+++ core/src/org/rexcellentgames/burningknight/entity/level/rooms/connection/RingConnectionRoom.java
@@ -2,6 +2,7 @@ package org.rexcellentgames.burningknight.entity.level.rooms.connection;
 
 import org.rexcellentgames.burningknight.entity.level.Level;
 import org.rexcellentgames.burningknight.entity.level.Terrain;
+import org.rexcellentgames.burningknight.entity.level.features.Door;
 import org.rexcellentgames.burningknight.entity.level.painters.Painter;
 import org.rexcellentgames.burningknight.util.MathUtils;
 import org.rexcellentgames.burningknight.util.Random;
@@ -21,18 +22,34 @@ public class RingConnectionRoom extends TunnelRoom {
 
 	@Override
 	public void paint(Level level) {
+
 		Painter.fill(level, this, Terrain.WALL);
 
 		if (Random.chance(50)) {
 			Painter.fill(level, this, 1, Terrain.CHASM);
 		}
 
-		super.paint(level);
+		byte fl = (Random.chance(25) ? (Random.chance(33) ? Terrain.CHASM : (Random.chance(50) ? Terrain.WALL : Terrain.LAVA))
+				: Terrain.randomFloor());
+
+		if (this.getWidth() > 4 && this.getHeight() > 4 && Random.chance(50)) {
+			this.paintTunnel(level, fl, true);
+		}
+
+		if (fl == Terrain.LAVA) {
+			this.paintTunnel(level, Terrain.randomFloor());
+		}
+
+		this.paintTunnel(level, (fl == Terrain.DIRT || fl == Terrain.LAVA) ? (Random.chance(50) ? Terrain.WATER : Terrain.DIRT) : Terrain.randomFloor());
+
+		for (Door door : this.connected.values()) {
+			door.setType(Door.Type.TUNNEL);
+		}
 
 		Rect ring = getConnectionSpace();
 		byte floor = Terrain.randomFloor();
 
-		Painter.fill(level, ring.left, ring.top, 3, 3, floor);
+		Painter.fill(level, ring.left, ring.top, 3, 3, fl == Terrain.LAVA ? (Random.chance(50) ? Terrain.WATER : Terrain.DIRT) : floor);
 		Painter.fill(level, ring.left + 1, ring.top + 1, 1, 1, Random.chance(50) ? Terrain.CHASM : Terrain.WALL);
 	}
 
