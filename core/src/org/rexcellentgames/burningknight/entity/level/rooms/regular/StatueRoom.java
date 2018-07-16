bb66d319337288f493d118d7423146230aa158d8
Egor Dorichev <egordorichev@gmail.com>
2018-07-16 10:44:12 +0300

    More statue room variation

--- core/src/org/rexcellentgames/burningknight/entity/level/rooms/regular/StatueRoom.java
+++ core/src/org/rexcellentgames/burningknight/entity/level/rooms/regular/StatueRoom.java
@@ -2,23 +2,47 @@ package org.rexcellentgames.burningknight.entity.level.rooms.regular;
 
 import org.rexcellentgames.burningknight.Dungeon;
 import org.rexcellentgames.burningknight.entity.level.Level;
+import org.rexcellentgames.burningknight.entity.level.Terrain;
 import org.rexcellentgames.burningknight.entity.level.entities.Statue;
+import org.rexcellentgames.burningknight.entity.level.painters.Painter;
 import org.rexcellentgames.burningknight.entity.level.save.LevelSave;
-import org.rexcellentgames.burningknight.Dungeon;
-import org.rexcellentgames.burningknight.entity.level.Level;
-import org.rexcellentgames.burningknight.entity.level.entities.Statue;
-import org.rexcellentgames.burningknight.entity.level.save.LevelSave;
+import org.rexcellentgames.burningknight.util.Random;
 
 public class StatueRoom extends RegularRoom {
 	@Override
 	public void paint(Level level) {
 		super.paint(level);
 
-		this.row(this.left + 2);
-		this.row(this.right - 2);
+		if (Random.chance(50)) {
+			this.collumn(this.left + 2);
+			this.collumn(this.right - 2);
+		} else {
+			this.row(this.top + 2);
+			this.row(this.bottom - 2);
+		}
+
+		if (Random.chance(50)) {
+			byte f = Terrain.CHASM;
+
+			if (Random.chance(50)) {
+				Painter.fillEllipse(level, this, 3, f);
+			} else {
+				Painter.fill(level, this, 3, f);
+			}
+
+			if (Random.chance(50)) {
+				f = Terrain.WALL;
+
+				if (Random.chance(50)) {
+					Painter.fillEllipse(level, this, 5, f);
+				} else {
+					Painter.fill(level, this, 5, f);
+				}
+			}
+		}
 	}
 
-	private void row(int x) {
+	private void collumn(int x) {
 		for (int y = this.top + 2; y < this.bottom - 1; y += 3) {
 			Statue statue = new Statue();
 
@@ -30,13 +54,25 @@ public class StatueRoom extends RegularRoom {
 		}
 	}
 
-	@Override
-	public int getMinWidth() {
-		return 7;
+	private void row(int y) {
+		for (int x = this.left + 2; x < this.right - 1; x += 2) {
+			Statue statue = new Statue();
+
+			statue.x = x * 16;
+			statue.y = y * 16;
+
+			Dungeon.area.add(statue);
+			LevelSave.add(statue);
+		}
 	}
 
 	@Override
-	public int getMinHeight() {
-		return 7;
+	protected int validateWidth(int w) {
+		return w - w % 2 + 1;
+	}
+
+	@Override
+	protected int validateHeight(int h) {
+		return h - h % 2 + 1;
 	}
 }
