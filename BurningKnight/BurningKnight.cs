04b4540823281b3ea1f6453dc291bcd3eb3c6050
egordorichev <egordorichev@gmail.com>
2018-07-22 16:29:17 +0300

    Starting C# branch

---
+++ BurningKnight/BurningKnight.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Core
+{
+    public class BurningKnight : Game
+    {
+        public GraphicsDeviceManager graphicsDevice;
+        public SpriteBatch spriteBatch;
+        
+        public BurningKnight()
+        {
+            graphicsDevice = new GraphicsDeviceManager(this)
+            {
+                PreferMultiSampling = true
+            };
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ BurningKnight/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new BurningKnight())
+            {
+                game.Run();
+            }
+        }
+    }
+}
