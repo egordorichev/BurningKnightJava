88df3b1ec292aee9519a11de3835d7f04a1c1bd7
Egor Dorichev <egordorichev@gmail.com>
2018-08-06 10:00:03 +0300

    Gamepads + mouse are working

--- BurningKnight/Game/Inputs/Input.cs
+++ BurningKnight/Game/Inputs/Input.cs
@@ -11,41 +11,129 @@ namespace BurningKnight.Game.Inputs
 			HELD, RELEASED, PRESSED, UP
 		}
 		
-		private static Dictionary<string, List<Keys>> bindings = new Dictionary<string, List<Keys>>();
-		private static Dictionary<Keys, State> states = new Dictionary<Keys, State>();
-		private static Dictionary<Keys, State> next = new Dictionary<Keys, State>();
+		private static Dictionary<string, List<InputNames>> bindings = new Dictionary<string, List<InputNames>>();
+		private static Dictionary<InputNames, State> states = new Dictionary<InputNames, State>();
+		private static Dictionary<InputNames, State> next = new Dictionary<InputNames, State>();
 
 		public static void Init()
 		{
-			Bind("move_left", Keys.A);
-			Bind("move_left", Keys.Left);
+			Bind("move_left", InputNames.ControllerX);
 		}
 
-		public static void Bind(string id, Keys key)
+		public static void Bind(string id, InputNames key)
 		{
 			if (!bindings.ContainsKey(id))
 			{
-				bindings[id] = new List<Keys>();
+				bindings[id] = new List<InputNames>();
 			}
 			
 			bindings[id].Add(key);
 			states[key] = State.RELEASED;
 		}
 
+		private static int mouseWheelValue;
+		public static int MouseWheelValue => mouseWheelValue;
+
+		private static Vector2 leftStick;
+		public static Vector2 LeftStick => leftStick;
+
+		private static Vector2 rightStick;
+		public static Vector2 RightStick => rightStick;
+
 		public static void Update(float dt)
 		{
 			KeyboardState state = Keyboard.GetState();
-			/*MouseState mouse = Mouse.GetState();
+			MouseState mouse = Mouse.GetState();
 
-			GamePadState gamepad = GamePad.GetState(PlayerIndex.One, 
-				GamePadDeadZone.Circular);*/	
+			mouseWheelValue = mouse.ScrollWheelValue;
 			
+			GamePadState gamepad = GamePad.GetState(PlayerIndex.One, 
+				GamePadDeadZone.Circular);
 			
 			next.Clear();
 			
 			foreach (var pair in states)
 			{
-				bool down = state.IsKeyDown(pair.Key);
+				bool down = false;
+
+				switch (pair.Key)
+				{
+					case InputNames.MouseWheel:
+						down = mouse.ScrollWheelValue != 0;
+						break;
+					case InputNames.MouseRight:
+						down = mouse.RightButton == ButtonState.Pressed;
+						break;
+					case InputNames.MouseLeft:
+						down = mouse.LeftButton == ButtonState.Pressed;
+						break;
+					case InputNames.MouseMiddle:
+						down = mouse.MiddleButton == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerA:
+						down = gamepad.IsConnected && gamepad.Buttons.A == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerB:
+						down = gamepad.IsConnected && gamepad.Buttons.B == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerX:
+						down = gamepad.IsConnected && gamepad.Buttons.X == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerY:
+						down = gamepad.IsConnected && gamepad.Buttons.Y == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerStart:
+						down = gamepad.IsConnected && gamepad.Buttons.Start == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerBack:
+						down = gamepad.IsConnected && gamepad.Buttons.B == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerLeftStick:
+						down = gamepad.IsConnected && gamepad.Buttons.LeftStick == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerRightStick:
+						down = gamepad.IsConnected && gamepad.Buttons.RightStick == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerLeftShoulder:
+						down = gamepad.IsConnected && gamepad.Buttons.LeftShoulder == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerRightShoulder:
+						down = gamepad.IsConnected && gamepad.Buttons.RightShoulder == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerDLeft:
+						down = gamepad.IsConnected && gamepad.DPad.Left == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerDRight:
+						down = gamepad.IsConnected && gamepad.DPad.Right == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerDUp:
+						down = gamepad.IsConnected && gamepad.DPad.Up == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerDDown:
+						down = gamepad.IsConnected && gamepad.DPad.Down == ButtonState.Pressed;
+						break;
+					case InputNames.ControllerLeftTrigger:
+						// down = gamepad.IsConnected && gamepad.Triggers.Left ???? float ????;
+						break;
+					case InputNames.ControllerRightTrigger:
+						// down = false;
+						break;
+					case InputNames.ControllerAxisLeft:
+						if (gamepad.IsConnected)
+						{
+							leftStick = gamepad.ThumbSticks.Left;
+						}
+						break;
+					case InputNames.ControllerAxisRight: 
+						if (gamepad.IsConnected)
+						{
+							rightStick = gamepad.ThumbSticks.Right;
+						}
+						break;
+					default: 
+						down = state.IsKeyDown((Keys) pair.Key);
+						break;
+				}
 
 				if (down)
 				{
