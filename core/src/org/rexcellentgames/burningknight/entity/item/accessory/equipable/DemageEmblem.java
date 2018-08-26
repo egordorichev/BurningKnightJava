dc7ee3ca93cb59bccbf1026efd717cf7b2833396
Egor Dorichev <egordorichev@gmail.com>
2018-08-26 20:26:09 +0300

    Done for today

--- core/src/org/rexcellentgames/burningknight/entity/item/accessory/equipable/DemageEmblem.java
+++ core/src/org/rexcellentgames/burningknight/entity/item/accessory/equipable/DemageEmblem.java
@@ -1,7 +1,6 @@
 package org.rexcellentgames.burningknight.entity.item.accessory.equipable;
 
 import org.rexcellentgames.burningknight.assets.Locale;
-import org.rexcellentgames.burningknight.entity.creature.player.Player;
 
 public class DemageEmblem extends Equipable {
 	{
@@ -13,20 +12,14 @@ public class DemageEmblem extends Equipable {
 	@Override
 	public void onEquip(boolean load) {
 		super.onEquip(load);
-
-		if (this.owner instanceof Player) {
-			((Player) this.owner).defenseModifier += 0.2f;
-			((Player) this.owner).damageModifier += 0.2f;
-		}
+		this.owner.defenseModifier += 0.2f;
+		this.owner.damageModifier += 0.2f;
 	}
 
 	@Override
 	public void onUnequip(boolean load) {
 		super.onUnequip(load);
-
-		if (this.owner instanceof Player) {
-			((Player) this.owner).defenseModifier -= 0.2f;
-			((Player) this.owner).damageModifier -= 0.2f;
-		}
+		this.owner.defenseModifier -= 0.2f;
+		this.owner.damageModifier -= 0.2f;
 	}
 }
