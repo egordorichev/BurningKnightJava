3a155a1d78f4e7623880e4e0bd67c1625ac07721
egordorichev <egordorichev@gmail.com>
2018-08-27 18:18:59 +0300

    Mimic totem and mimic summoner

--- core/src/org/rexcellentgames/burningknight/entity/item/accessory/equippable/MimicSummoner.kt
+++ core/src/org/rexcellentgames/burningknight/entity/item/accessory/equippable/MimicSummoner.kt
@@ -1,7 +1,9 @@
 package org.rexcellentgames.burningknight.entity.item.accessory.equippable
 
+import org.rexcellentgames.burningknight.assets.Locale
 import org.rexcellentgames.burningknight.entity.level.entities.chest.Chest
 import org.rexcellentgames.burningknight.entity.level.entities.chest.Mimic
+import org.rexcellentgames.burningknight.util.Random
 
 class MimicSummoner : Equippable() {
 	init {
@@ -10,15 +12,42 @@ class MimicSummoner : Equippable() {
 
 	override fun onEquip(load: Boolean) {
 		super.onEquip(load)
-		Mimic.chance += 100f
+		Mimic.chance += getChance()
 
 		for (chest in Chest.all) {
-			chest.toMimic()
+			if (Random.chance(Mimic.chance)) {
+				chest.toMimic()
+			}
 		}
 	}
 
 	override fun onUnequip(load: Boolean) {
 		super.onUnequip(load)
-		Mimic.chance -= 100f
+		Mimic.chance -= getChance()
+	}
+
+	override fun upgrade() {
+		Mimic.chance -= getChance()
+		level ++
+		Mimic.chance += getChance()
+	}
+
+	fun getChance(): Float {
+		return this.level * 20 + 40f
+	}
+
+	override fun getMaxLevel(): Int {
+		return 3
+	}
+
+	override fun getDescription(): String {
+		var d = super.getDescription().replace("{CHANCE}", getChance().toInt().toString())
+
+		d = d.replace("{ALMOST}", if (getChance() < 100f) almost else "")
+		return d;
+	}
+
+	companion object {
+		val almost = " " + Locale.get("almost")
 	}
 }
