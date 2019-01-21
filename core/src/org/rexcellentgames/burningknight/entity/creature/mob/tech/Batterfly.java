8ddf3280e07736feafaa3c853cd2e3ca1c793074
Egor Dorichev <egordorichev@gmail.com>
2019-01-21 13:56:01 +0300

    Blood area mob mockups

---
+++ core/src/org/rexcellentgames/burningknight/entity/creature/mob/tech/Batterfly.java
@@ -0,0 +1,7 @@
+package org.rexcellentgames.burningknight.entity.creature.mob.tech;
+
+import org.rexcellentgames.burningknight.entity.creature.mob.common.Fly;
+
+public class Batterfly extends Fly {
+
+}
